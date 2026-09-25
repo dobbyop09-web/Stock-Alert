085f896c9c74991bf1c49186d5802001f096fdc7
=Tushar <=tushar09821.gmail.com>
2026-09-25 15:59:44 +0530

    Toggle feature to toggle between nse api call or kotak if any fails

--- src/main/java/com/dobby/price_alert/runners/KotakTestRunner.java
+++ src/main/java/com/dobby/price_alert/runners/KotakTestRunner.java
@@ -3,9 +3,9 @@ package com.dobby.price_alert.runners;
 import com.dobby.price_alert.client.KotakSession;
 import com.dobby.price_alert.constants.SheetType;
 import com.dobby.price_alert.dto.KotakScrip;
-import com.dobby.price_alert.dto.kotak.KotakQuoteResponse;
 import com.dobby.price_alert.dto.kotak.KotakTotpLoginResponse;
 import com.dobby.price_alert.service.*;
+import com.dobby.price_alert.service.kotak.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
@@ -61,7 +61,7 @@ public class KotakTestRunner implements CommandLineRunner {
 //            System.out.println("Kotak MPIN validation successful.");
 //            System.out.println("Trade session received.");
 //        }
-//
+
 //
 //        ClassPathResource resource =
 //                new ClassPathResource("nse_cm-v1.csv");
@@ -75,43 +75,48 @@ public class KotakTestRunner implements CommandLineRunner {
 //                    StandardCharsets.UTF_8
 //            );
 //        }
+//        System.out.println(kotakScripMasterApiService.getMasterScripFilePaths(session));
+//
+//
 //
 //        System.out.println("NSE master loaded from resources.");
 //
-//        List<KotakScrip> scrips =
-//                scripMasterParser.parse(nseMaster);
-//        for (KotakScrip scrip : scrips) {
-//
-//            if ("BSE".equals(scrip.getSymbolName())) {
-//
-//                System.out.println("TCS found:");
-//                System.out.println(
-//                        "Symbol: " + scrip.getSymbol()
-//                );
-//                System.out.println(
-//                        "Exchange Segment: "
-//                                + scrip.getExchangeSegment()
-//                );
-//                System.out.println(
-//                        "Trading Symbol: "
-//                                + scrip.getTradingSymbol()
-//                );
-//                System.out.println(
-//                        "ISIN: " + scrip.getIsin()
-//                );
-//
-//                break;
-//            }
-//        }
+////        List<KotakScrip> scrips =
+////                scripMasterParser.parse(nseMaster);
+////        for (KotakScrip scrip : scrips) {
+////
+////            if ("BSE".equals(scrip.getSymbolName())) {
+////
+////                System.out.println("TCS found:");
+////                System.out.println(
+////                        "Symbol: " + scrip.getSymbol()
+////                );
+////                System.out.println(
+////                        "Exchange Segment: "
+////                                + scrip.getExchangeSegment()
+////                );
+////                System.out.println(
+////                        "Trading Symbol: "
+////                                + scrip.getTradingSymbol()
+////                );
+////                System.out.println(
+////                        "ISIN: " + scrip.getIsin()
+////                );
+////
+////                break;
+////            }
+////        }
 //        Map<String, KotakScrip> scripMap =
 //                kotakScripLookupService.loadNseScripMap();
-//        for (SheetType sheet : SheetType.values()) {
-//
-//            csvReaderService.testKotakForCsvStocks(sheet.getSheetConfig(),scripMap);
 //
 //
-//        }
-
+////        for (SheetType sheet : SheetType.values()) {
+////
+////            csvReaderService.testKotakForCsvStocks(sheet.getSheetConfig(),scripMap);
+////
+////        }
+//       List<String> tokens =  List.of("544937");
+//           kotakQuoteService.getOhlc("bse_cm",tokens);
 
 
     }
